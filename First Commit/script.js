140ba4d1d6843807a2b59f5b6e07f07efe805413
prakharsahu020 <prakharsahu020@gmail.com>
2026-03-17 23:18:38 +0530

    First Commit

---
+++ First Commit/script.js
@@ -0,0 +1,35 @@
+var a = 100;
+var b = 13;
+var lineBreak = '<br>';
+
+document.write('a + b + c = ');
+var result = a + b + c;
+
+document.write(result);
+document.write(lineBreak);
+
+
+
+
+document.write('a - b - c = ');
+var result = a - b - c;
+
+document.write(result);
+document.write(lineBreak);
+
+
+
+document.write('a * b * c = ');
+var result = a * b * c;
+
+document.write(result);
+document.write(lineBreak);
+
+
+
+
+document.write('a / b / c = ');
+var result = a / b / c;
+
+document.write(result);
+document.write(lineBreak);
